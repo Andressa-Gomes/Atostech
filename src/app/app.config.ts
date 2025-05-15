54a322eb05cf46e72a6eb786d17f6a8c22b77e10
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2025-05-15 00:18:28 -0300

    feat: add HashLocation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes), provideClientHydration(withEventReplay()),
-     provideHttpClient()]
+     provideRouter(routes), 
+     provideClientHydration(withEventReplay()),
+     provideHttpClient(),
+     {provide: LocationStrategy, useClass: HashLocationStrategy }]
 };
