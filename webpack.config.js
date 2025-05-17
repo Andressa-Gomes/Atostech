4bd9fe8a24b087dbe21cac13a43a5f1a022b8017
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2025-05-17 19:25:00 -0300

    build: add cache:false

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   optimization: {
     minimize: true,
+    cache: false, // desativa cache, pesa menos em RAM
     minimizer: [
       new TerserPlugin({
         parallel: false, // desativa múltiplos threads, pesa menos em RAM
